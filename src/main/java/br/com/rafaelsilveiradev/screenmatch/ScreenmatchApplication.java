3a7efde27eef970bd35cbaec4a9fd34b0f7f464f
Rafael Ramalho Silveira <rafaelramalhosilveira@gmail.com>
2024-04-03 06:19:46 -0400

    Inicio do Projeto Spring

---
+++ src/main/java/br/com/rafaelsilveiradev/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,26 @@
+package br.com.rafaelsilveiradev.screenmatch;
+
+import br.com.rafaelsilveiradev.screenmatch.model.DadosSerie;
+import br.com.rafaelsilveiradev.screenmatch.services.ConsumoAPI;
+import br.com.rafaelsilveiradev.screenmatch.services.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoAPI();
+		var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=123e42a0&");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
