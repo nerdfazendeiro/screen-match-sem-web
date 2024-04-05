57dc3828e8e8f293ff2de09440ece3fb58db43e2
Rafael Ramalho Silveira <rafaelramalhosilveira@gmail.com>
2024-04-05 06:07:45 -0400

    stream para filtrar top 5 episodios

--- src/main/java/br/com/rafaelsilveiradev/screenmatch/principal/Principal.java
+++ src/main/java/br/com/rafaelsilveiradev/screenmatch/principal/Principal.java
@@ -7,8 +7,10 @@ import br.com.rafaelsilveiradev.screenmatch.services.ConsumoAPI;
 import br.com.rafaelsilveiradev.screenmatch.services.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -38,5 +40,17 @@ public class Principal {
 		temporadas.forEach(System.out::println);
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
+
     }
 }
