2e5e10d9968b9928b1ad645b8dc34aec864585ad
Rafael Ramalho Silveira <rafaelramalhosilveira@gmail.com>
2024-04-09 06:07:01 -0400

    Buscando episodios por data

--- src/main/java/br/com/rafaelsilveiradev/screenmatch/principal/Principal.java
+++ src/main/java/br/com/rafaelsilveiradev/screenmatch/principal/Principal.java
@@ -7,6 +7,9 @@ import br.com.rafaelsilveiradev.screenmatch.model.Episodio;
 import br.com.rafaelsilveiradev.screenmatch.services.ConsumoAPI;
 import br.com.rafaelsilveiradev.screenmatch.services.ConverteDados;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -59,5 +62,22 @@ public class Principal {
                 ).collect(Collectors.toList());
 
         episodios.forEach(System.out::println);
+
+        System.out.println("A partir de qual ano você deseja ver os episódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
+
+        LocalDate dataBusca = LocalDate.of(ano, 1,1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada: "+ e.getTemporada() +
+                                " Episoódio: "+ e.getTitulo() +
+                                " Data lançamento: " + e.getDataLancamento().format(formatador)
+                ));
+
     }
 }
