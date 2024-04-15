c4db926c6027266b1990286c86049ce5e105991f
Rafael Ramalho Silveira <rafaelramalhosilveira@gmail.com>
2024-04-15 06:52:58 -0400

    Melhorando a impressão das média

--- src/main/java/br/com/rafaelsilveiradev/screenmatch/principal/Principal.java
+++ src/main/java/br/com/rafaelsilveiradev/screenmatch/principal/Principal.java
@@ -102,7 +102,10 @@ public class Principal {
         DoubleSummaryStatistics est = episodios.stream()
                 .filter(e -> e.getAvaliacao() > 0.0)
                 .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
-        System.out.println(est);
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
 
 
     }
